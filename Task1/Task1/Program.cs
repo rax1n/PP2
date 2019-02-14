56f223c7102c7861d5f1d3d7001d4d62e3244976
rax1n <rahmanmagametov82@gmail.com>
2019-02-14 23:19:24 +0600

    Added first Task

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); //создаю переменную типа инт, для первого числа. Число элементов в массиве
+            int[] arr = new int[n]; //создаю массив, чтобы вбить в неё "n" чисел через
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //--------------------------
+            int counter = 0;
+            int[] newarr = new int[n];
+            //--------------------------
+            for (int i = 0; i < n; i++)
+            {
+                int prime = 0;
+                if (arr[i] > 1)
+                {
+                    for (int j = 2; j < arr[i]; j++)
+                    {
+                        if (arr[i] % j != 0)
+                        {
+                            continue;
+                        } else { prime++; }
+                    }
+                    if (prime == 0)
+                    {
+                        newarr[counter] = arr[i];
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+            for(int i = 0;i<counter;i++)
+            {
+                Console.Write(newarr[i] + " ");
+            }
+        }
+    }
+}
